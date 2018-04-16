4e2bd238d437aca738636f2a6258aecf30aa455f
Archana Bansal <archanamittal0388@gmail.com>
2018-04-16 12:13:27 +0530

    Feat: Components folder created to keep all components

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SignUp from './components/signup'
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'))
+ReactDOM.render(<SignUp />, document.getElementById('root'))
