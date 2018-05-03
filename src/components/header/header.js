1a732016dfff2a47898b29856538f0e931339a6c
Abhicoding <shinganeabhishek@gmail.com>
2018-05-03 12:18:58 +0530

    Added a logout button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import GoogleOauth from '../googleOauth'
+import Logout from '../logout'
 import './style.css'
 
 const Header = ({isLoggedin, profile, onLoginSuccess}) => {
+  console.log(isLoggedin)
   return (
     <div className='header'>
       <div>
@@ -13,8 +15,11 @@ const Header = ({isLoggedin, profile, onLoginSuccess}) => {
       </div>
       {!isLoggedin && <GoogleOauth onLoginSuccess={onLoginSuccess} />}
       {isLoggedin &&
-      <div className='header__user-info'>
-        <img className='header__user-image' src={profile.getImageUrl()} />{profile.getName()}
+      <div>
+        <div className='header__user-info'>
+          <img className='header__user-image' src={profile.getImageUrl()} />{profile.getName()}
+        </div>
+        <Logout />
       </div>
       }
     </div>
---
+++ src/components/logout/logout.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+import './style.css'
+class Logout extends Component {
+  render () {
+    return (
+      <button className='logout'>logout</button>
+    )
+  }
+}
+
+export default Logout
