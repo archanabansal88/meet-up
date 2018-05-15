3e5f295b54729d4ea1082b8f5e05b390b4d490f0
Archana Bansal <archanamittal0388@gmail.com>
2018-05-15 09:49:19 +0530

    Feat: Created Pop up component

---
+++ src/shared/popup/popup.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Button from '../button'
+import './style.css'
+
+const PopUp = props => {
+  return (
+    <section className='ui-component__popup'>
+      <div className='ui-component__popup-content'>
+        <Button label={String.fromCharCode(215)} onClick={props.onClose} />
+        {props.children}
+      </div>
+    </section>
+  )
+}
+
+export default PopUp
