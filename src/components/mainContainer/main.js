70141285e86ee23f6540760696924c76ac15534e
Abhicoding <shinganeabhishek@gmail.com>
2018-05-24 15:01:50 +0530

    updated packages

--- src/components/mainContainer/main.js
+++ src/components/mainContainer/main.js
@@ -70,7 +70,6 @@ class Main extends Component {
 
   render () {
     const {isLoggedin, profile, first} = this.state
-    console.log(profile, 'in Main')
     return (
       <BrowserRouter>
         <div>
