eecfb3b7d6029156f79221d46e51e036d630c9c5
Archana Bansal <archanamittal0388@gmail.com>
2018-06-08 10:13:15 +0530

    Feat: Added routes to redirect to edit event

--- src/components/admin/admin.js
+++ src/components/admin/admin.js
@@ -1,16 +1,29 @@
 import React, {Component} from 'react'
-import {Route} from 'react-router-dom'
-import Login from './login'
+import {Route, Redirect} from 'react-router-dom'
 import DashBoard from './dashboard'
-import CreateEvent from '../createEvent'
+import EventForm from '../eventForm'
 
 class Admin extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      event: false
+    }
+    this.handleEventClick = this.handleEventClick.bind(this)
+  }
+
+  handleEventClick (event) {
+    this.setState({event})
+  }
   render () {
+    const {event} = this.state
     return (
       <div>
-        <Route path='/' component={Login} />
-        <Route exact path='/admin/dashboard' component={DashBoard} />
-        <Route exact path='/admin/create' component={CreateEvent} />
+        {event && <Redirect to={`/admin/edit`} />}
+        <Route exact path='/admin' render={() => <h2> Please login as admin</h2>} />
+        <Route exact path='/admin/dashboard' render={() => <DashBoard onEventClick={this.handleEventClick} />} />
+        <Route exact path='/admin/create' component={EventForm} />
+        <Route exact path='/admin/edit' render={() => <EventForm {...event} isEditMode />} />
       </div>
     )
   }
--- src/components/admin/dashboard.js
+++ src/components/admin/dashboard.js
@@ -2,18 +2,10 @@ import React, {Component} from 'react'
 import EventContainer from '../eventContainer'
 
 class DashBoard extends Component {
-  constructor (props) {
-    super(props)
-    this.handleEventClick = this.handleEventClick.bind(this)
-  }
-
-  handleEventClick (event) {
-    console.log(event.id)
-  }
   render () {
     return (
       <div>
-        <EventContainer onEventClick={this.handleEventClick} />
+        <EventContainer onEventClick={this.props.onEventClick} />
       </div>
     )
   }
